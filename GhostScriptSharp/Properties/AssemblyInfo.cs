1cdb0153254dec13c758cbf42242054bd227deac
Matthew Ephraim <me@mattephraim.com>
2010-05-29 08:31:08 +0100

    Updated version number

--- GhostScriptSharp/Properties/AssemblyInfo.cs
+++ GhostScriptSharp/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.3")]
-[assembly: AssemblyFileVersion("0.3")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0")]
